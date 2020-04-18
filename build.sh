1a23aacc3449974ef41cac5460def22a005e7006
tala-abandeh <62711900+tala-abandeh@users.noreply.github.com>
2020-04-18 17:39:33 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ set -e
 # Define some path
 SRC=./web
 DST=/var/ww/html
-echo "Tests started at…"
-date "+%H:%M:%S"
+
 # Check if deployment destination exists
 if [ -d "$DST" ]; then
     echo "Deployment target exists: $DST"
     cp -r $SRC/* $DST
+    echo "Tests started at…"
+date "+%H:%M:%S"
 else
     echo "Deployment target does not exist."
     exit 2
