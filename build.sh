95181456c6175216cfe9dbc6d8e981e039c796a3
tala-abandeh <62711900+tala-abandeh@users.noreply.github.com>
2020-04-18 20:00:22 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,18 @@ DST=/var/ww/html
 if [ -d "$DST" ]; then
     echo "Deployment target exists: $DST"
     cp -r $SRC/* $DST
+   
+OUTPUT="$(date)"
+# You must add following two lines before
+# outputting data to the web browser from shell
+script |
+ echo "Content-type: text/html"
+ echo ""
+ echo "<html><head><title>Demo</title></head><body>"
+ echo "Today is $OUTPUT <br>"
+ echo "Current directory is $(pwd) <br>"
+ echo "Shell Script name is $0"
+ echo "</body></html>"
 else
     echo "Deployment target does not exist."
     exit 2
